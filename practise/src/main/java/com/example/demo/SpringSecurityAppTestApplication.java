4fdaa080481838632658ddcc17e091dd7a5c8b3b
Krieger <krieger@192-168-0-35.local>
2023-10-09 01:07:10 -0400

    I commit my life

---
+++ practise/src/main/java/com/example/demo/SpringSecurityAppTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAppTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAppTestApplication.class, args);
+	}
+
+}
